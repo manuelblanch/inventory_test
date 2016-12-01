9cca23ed6fd3a452e715f27735b187afa5164fb8
manuelblanch <manuelblanch@iesebre.com>
2016-12-01 20:38:45 +0100

    pujada

---
+++ app/Providers/InventoryServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class InventoryServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+        if (!defined(SCOOL_INVENTORY_PATH))
+    }
+}
