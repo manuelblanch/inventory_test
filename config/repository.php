45983af7a700e78ab7d537f28060a2c65c606438
manuelblanch <manuelblanch@iesebre.com>
2017-01-19 21:36:45 +0100

    pujada

--- config/repository.php
+++ config/repository.php
@@ -218,8 +218,9 @@ return [
             'presenters'   => 'Presenters',
             'validators'   => 'Validators',
             'controllers'  => 'Http/Controllers',
+            'routes'       => 'Http/routes.php',
             'requests'     => 'Http/Requests',
-            'provider'     => 'CurriculumServiceProvider',
+            'provider'     => 'InventoryServiceProvider',
             'criteria'     => 'Criteria',
         ]
     ]
