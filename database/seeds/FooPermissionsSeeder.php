c7a9e0c7d36a1fa2428800e456b721010915ab78
manuelblanch <manuelblanch@iesebre.com>
2017-01-26 19:59:11 +0100

    creacio fitxerpermision model

---
+++ database/seeds/FooPermissionsSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class FooPermissionsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
