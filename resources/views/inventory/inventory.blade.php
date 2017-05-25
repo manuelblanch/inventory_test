3e74bd1ae13684e34b11e92d53c6fa0328ffbc91
manuelblanch <manuelblanch@iesebre.com>
2017-05-25 14:34:57 +0200

    creating view

---
+++ resources/views/inventory/inventory.blade.php
@@ -0,0 +1,16 @@
+@extends('adminlte::layouts.app')
+
+@section('htmlheader_title')
+    <h2>Inventory</h2>
+@endsection
+
+
+@section('main-content')
+    <!-- ========================================================================================================== -->
+    <!-- Main content -->
+    <section class="content">
+        <a href="inventory/create" class="btn btn-primary">Afegir objecte a inventari</a>
+
+    </section>
+
+@endsection
