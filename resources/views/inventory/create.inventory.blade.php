aaa1f0347fbcf3ca9dcc3dda8d14c802bcb6e7eb
manuelblanch <manuelblanch@iesebre.com>
2017-05-26 21:52:47 +0200

    creating views

--- resources/views/inventory/create.inventory.blade.php
+++ resources/views/inventory/create.inventory.blade.php
@@ -9,12 +9,12 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Afegeix un nou item a l'inventari</div>
+                <div class="panel-heading">Control Inventari</div>
                 <div class="panel-body">
                     <form class="form-horizontal" role="form" method="POST" action="{{ route('') }}" enctype="multipart/form-data">
                         @{{ csrf_field() }}
-                        <div class="form-group{{ $errors->has('Nom') ? ' has-error' : '' }}">
-                            <label for="nom" class="col-md-4 control-label">Nom</label>
+                        <div class="form-group{{ $errors->has('Id') ? ' has-error' : '' }}">
+                            <label for="id" class="col-md-4 control-label">Id</label>
 
                             <div class="col-md-6">
                                 <input id="firstname" type="text" class="form-control" name="firstname" value="{{ old('firstname') }}" required autofocus>
@@ -26,6 +26,32 @@
                                 @endif
                             </div>
                         </div>
+                        <div class="form-group{{ $errors->has('public-id') ? ' has-error' : '' }}">
+                            <label for="middlename" class="col-md-4 control-label">Public Id</label>
+
+                            <div class="col-md-6">
+                                <input id="public id" type="text" class="form-control" name="public id" value="{{ old('public id') }}" required>
+
+                                @if ($errors->has('public id'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('public id') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                          </div>
+                          <div class="form-group{{ $errors->has('nom') ? ' has-error' : '' }}">
+                              <label for="nom" class="col-md-4 control-label">Nom</label>
+
+                              <div class="col-md-6">
+                                  <input id="nom" type="text" class="form-control" name="nom" value="{{ old('nom') }}" required>
+
+                                  @if ($errors->has('nom'))
+                                      <span class="help-block">
+                                          <strong>{{ $errors->first('nom') }}</strong>
+                                      </span>
+                                  @endif
+                              </div>
+                            </div>
                         <div class="form-group">
                             <label for="avatar" class="col-md-4 control-label" >Imatge</label>
                             <div class="col-md-6">
@@ -35,7 +61,7 @@
                         <div class="form-group">
                             <div class="col-md-6 col-md-offset-4">
                                 <button type="submit" class="btn btn-primary">
-                                    Create
+                                    Introdueix imatge de l'objecte a l'inventari
                                 </button>
                             </div>
                         </div>
--- resources/views/vue.blade.php
+++ resources/views/vue.blade.php
@@ -5,17 +5,22 @@
   <title>Intro a VueJS</title>
 </head>
 <body>
-  <div id="app">
-  <h1>@{{ greeting }}</h1>
-  <input type="text" v-model="greeting">
-</div>
+  <div id="app-5">
+    <p>@{{ message }}</p>
+    <button v-on:click="reverseMessage">Reverse Message</button>
+  </div>
   <script src="https://unpkg.com/vue"></script>
   <script>
-  new Vue({
-elç: '#app',
-data: {
-  greeting: 'Hola Vue!'
-}
+  var app5 = new Vue({
+  el: '#app-5',
+  data: {
+    message: 'Hello Vue.js!'
+  },
+  methods: {
+    reverseMessage: function () {
+      this.message = this.message.split('').reverse().join('')
+    }
+  }
 })
   </script>
 </body>
