3744cce7c23753fe6c70b75bdc43ef7ac5fcb115
manuelblanch <manuelblanch@iesebre.com>
2017-06-04 13:02:21 +0200

    creating view export

--- app/Http/Controllers/ExportController.php
+++ app/Http/Controllers/ExportController.php
@@ -47,7 +47,7 @@ class ExportController extends Controller
         ->leftJoin('provider', 'inventories.provider_id', '=', 'provider.id')
         ->select('inventories.*', 'material_type.name as material_type_name', 'material_type.id as material_type_id', 'brand.name as brand_name', 'brand.id as brand_id', 'brand_model.name as brand_model_name', 'brand_model.id as model_id', 'location.name as location_name', 'location.id as location_id', 'moneySource.name as moneySource_name', 'moneySource.id as moneySourceId',
         'provider.name as provider_name', 'provider.id as provider_id')
-        ->paginate(5);
+        ->paginate();
         return view('/export/index', ['inventories' =>$inventories, 'searchingVals' => $constraints]);
 
         //$inventories = $this->getItemsInventory($constraints);
