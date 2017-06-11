8a134b938cd6cc681b20cee3ceadddb2f1c2050c
manuelblanch <manuelblanch@iesebre.com>
2017-06-11 23:36:56 +0200

    socialite

--- app/Http/Controllers/Auth/SocialAuthController.php
+++ app/Http/Controllers/Auth/SocialAuthController.php
@@ -9,48 +9,47 @@ use Laravel\Socialite\Facades\Socialite;
 class SocialAuthController extends Controller
 {
   public function redirectToProvider()
-{
-  return Socialite::driver($provider)->redirect();
-}
+      {
+          return Socialite::driver('github')->redirect();
+      }
 
+      /**
+       * Obtain the user information from GitHub.
+       *
+       * @return Response
+       */
+      public function handleProviderCallback()
+      {
+          try {
+              $user = Socialite::driver('github')->user();
+          } catch (Exception $e) {
+              return Redirect::to('auth/github');
+          }
 
-    /**
-     * Obtain the user information from GitHub.
-     *
-     * @return Response
-     */
-    public function handleProviderCallback($provider)
-    {
-        try {
-            $user = Socialite::driver($provider)->stateless()->user();
-        } catch (Exception $e) {
-            return Redirect::to('auth/github');
-        }
+          $authUser = $this->findOrCreateUser($user);
 
-        $authUser = $this->findOrCreateUser($user);
+          Auth::login($authUser, true);
 
-        Auth::login($authUser, true);
+          return Redirect::to('home');
+      }
 
-        return Redirect::to('home');
+      /**
+       * Return user if exists; create and return if doesn't
+       *
+       * @param $githubUser
+       * @return User
+       */
+      private function findOrCreateUser($githubUser)
+      {
+          if ($authUser = User::where('github_id', $githubUser->id)->first()) {
+              return $authUser;
+          }
+
+          return User::create([
+              'name' => $githubUser->name,
+              'email' => $githubUser->email,
+              'github_id' => $githubUser->id,
+              'avatar' => $githubUser->avatar
+          ]);
+      }
     }
-
-    /**
-     * Return user if exists; create and return if doesn't
-     *
-     * @param $githubUser
-     * @return User
-     */
-    private function findOrCreateUser($githubUser)
-    {
-        if ($authUser = User::where('github_id', $githubUser->id)->first()) {
-            return $authUser;
-        }
-
-        return User::create([
-            'name' => $githubUser->name,
-            'email' => $githubUser->email,
-            'github_id' => $githubUser->id,
-            'avatar' => $githubUser->avatar
-        ]);
-    }
-  }
