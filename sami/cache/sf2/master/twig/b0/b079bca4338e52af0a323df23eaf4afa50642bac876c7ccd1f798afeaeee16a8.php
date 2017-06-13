05b2d23378e2240010cf6e8e97ede7b0bff864d3
manuelblanch <manuelblanch@iesebre.com>
2017-06-13 23:59:34 +0200

    api

---
+++ sami/cache/sf2/master/twig/b0/b079bca4338e52af0a323df23eaf4afa50642bac876c7ccd1f798afeaeee16a8.php
@@ -0,0 +1,91 @@
+<?php
+
+/* classes.twig */
+class __TwigTemplate_1cbac9212506c3d7b4da37623b42f248d9047e08c78aca8e4217f6f22259661f extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        // line 1
+        $this->parent = $this->loadTemplate("layout/layout.twig", "classes.twig", 1);
+        $this->blocks = array(
+            'title' => array($this, 'block_title'),
+            'body_class' => array($this, 'block_body_class'),
+            'page_content' => array($this, 'block_page_content'),
+        );
+    }
+
+    protected function doGetParent(array $context)
+    {
+        return "layout/layout.twig";
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 2
+        $context["__internal_0a01baf10809972530391e1683bcb1cc37a93fb95fcd42b1f06bff17a677ebe7"] = $this->loadTemplate("macros.twig", "classes.twig", 2);
+        // line 1
+        $this->parent->display($context, array_merge($this->blocks, $blocks));
+    }
+
+    // line 3
+    public function block_title($context, array $blocks = array())
+    {
+        echo "All Classes | ";
+        $this->displayParentBlock("title", $context, $blocks);
+    }
+
+    // line 4
+    public function block_body_class($context, array $blocks = array())
+    {
+        echo "classes";
+    }
+
+    // line 6
+    public function block_page_content($context, array $blocks = array())
+    {
+        // line 7
+        echo "    <div class=\"page-header\">
+        <h1>Classes</h1>
+    </div>
+
+    ";
+        // line 11
+        echo $context["__internal_0a01baf10809972530391e1683bcb1cc37a93fb95fcd42b1f06bff17a677ebe7"]->macro_render_classes((isset($context["classes"]) || array_key_exists("classes", $context) ? $context["classes"] : (function () { throw new Twig_Error_Runtime('Variable "classes" does not exist.', 11, $this->getSourceContext()); })()));
+        echo "
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "classes.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  55 => 11,  49 => 7,  46 => 6,  40 => 4,  33 => 3,  29 => 1,  27 => 2,  11 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Twig_Source("{% extends \"layout/layout.twig\" %}
+{% from \"macros.twig\" import render_classes %}
+{% block title %}All Classes | {{ parent() }}{% endblock %}
+{% block body_class 'classes' %}
+
+{% block page_content %}
+    <div class=\"page-header\">
+        <h1>Classes</h1>
+    </div>
+
+    {{ render_classes(classes) }}
+{% endblock %}
+", "classes.twig", "phar:///home/manuel/Code/scool/inventory_test/sami.phar/Sami/Resources/themes/default/classes.twig");
+    }
+}
