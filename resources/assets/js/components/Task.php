b8a4104faa5bde311442df8ffc065c779d7f19f6
manuelblanch <manuelblanch@iesebre.com>
2017-10-20 22:22:11 +0200

    testing

--- resources/assets/js/components/Task.php
+++ resources/assets/js/components/Task.php
@@ -4,13 +4,13 @@
         <div class="row">
             <div class="col-md-12">
                 <div class="panel panel-default">
-                    <div class="panel-heading">My Tasks</div>
+                    <div class="panel-heading">Tasques</div>
 <template>
     <div class="container">
         <div class="row">
             <div class="col-md-12">
                 <div class="panel panel-default">
-                    <div class="panel-heading">My Tasks</div>
+                    <div class="panel-heading">Tasques</div>
 
                     <div class="panel-body">
 
