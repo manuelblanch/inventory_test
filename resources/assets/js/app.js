5506c044248753ae7b173b892f694e3d137fe903
manuelblanch <manuelblanch@iesebre.com>
2017-11-08 19:22:36 +0100

    testing

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -13,7 +13,7 @@ require('./bootstrap');
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-Vue.component('example', require('./components/Example.vue'));
+Vue.component('task', require('./components/Task.vue'));
 
 const app = new Vue({
     el: '#app'
