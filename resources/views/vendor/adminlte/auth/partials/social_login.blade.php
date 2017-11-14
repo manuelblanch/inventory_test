fbe43543e29957d4701ee4f20367e68ea75c454a
manuelblanch <manuelblanch@iesebre.com>
2017-11-14 18:18:40 +0100

    socialite twitter

--- resources/views/vendor/adminlte/auth/partials/social_login.blade.php
+++ resources/views/vendor/adminlte/auth/partials/social_login.blade.php
@@ -1,12 +1,7 @@
 <div class="social-auth-links text-center">
     <p>- OR -</p>
     <a href="{{ url('/auth/github') }}" class="btn btn-block btn-social btn-github btn-flat"><i class="fa fa-github"></i> {{ trans('adminlte_lang::message.signGithub') }}</a>
-    <a href="{{ url('/auth/github') }}" class="btn btn-block btn-social btn-github btn-flat"><i class="fa fa-github"></i> {{ trans('adminlte_lang::message.signGithub') }}</a>
-
-    <div class="form-group">
-        <div class="col-md-8 col-md-offset-4">
-            <center><a href="{{url('/auth/twitter')}}" class="btn btn-primary">Sign in with Twitter</a></center>
-          </div>
-      </div>
+    <a href="{{ url('/auth/twitter') }}" class="btn btn-block btn-social btn-twitter btn-flat"><i class="fa fa-twitter"></i> {{ trans('adminlte_lang::message.signTwitter') }}</a>
+    
 
 </div><!-- /.social-auth-links -->
