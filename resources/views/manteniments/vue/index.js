84e4dbb338b31b4d31f3ad802ebd68b0e0bf9a03
manuelblanch <manuelblanch@iesebre.com>
2017-11-23 19:58:21 +0100

    testing

--- resources/views/manteniments/vue/index.js
+++ resources/views/manteniments/vue/index.js
@@ -24,4 +24,21 @@ var Notify = {
   }
 }
 
+styles () {
+      let { x, y } = listToDirection(this.position)
+      let width = this.actualWidth.value
+      let suffix = this.actualWidth.type
+      let styles = {
+        width: width + suffix,
+        [y]: '0px'
+      }
+      if (x === 'center') {
+        styles['left'] = `calc(50% - ${width/2}${suffix})`
+      } else {
+        styles[x] = '0px'
+      }
+      return styles
+    },
+
+
 export default Notify
