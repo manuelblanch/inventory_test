a2a8a52a491026747f49cfd2b9ed41dcc7a8e707
manuelblanch <manuelblanch@iesebre.com>
2017-11-27 17:03:23 +0100

    testing

--- app/Events/PostPublished.php
+++ app/Events/PostPublished.php
@@ -10,27 +10,27 @@ use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 
-class PostPublished
-{
-    use Dispatchable, InteractsWithSockets, SerializesModels;
+class PostPublished implements ShouldBroadcast {
+  use Dispatchable, InteractsWithSockets, SerializesModels;
 
-    /**
-     * Create a new event instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        //
-    }
+  public $post;
 
-    /**
-     * Get the channels the event should broadcast on.
-     *
-     * @return Channel|array
-     */
-    public function broadcastOn()
-    {
-        return new PrivateChannel('channel-name');
-    }
+  public function __construct($post) {
+    $this->post = $post;
+  }
+
+  /**
+   * Get the channels the event should broadcast on.
+   *
+   * @return Channel|array
+   */
+  public function broadcastOn() {
+    return new Channel('posts');
+  }
+
+  public function broadcastWith() {
+    return [
+      'title' => $this->post->title,
+    ];
+  }
 }
--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Events\PostPublished;
 use App\Post;
 
 class PostController extends Controller {
@@ -17,11 +17,16 @@ class PostController extends Controller {
     // with $this->validate($request, $rules)
     // ...
 
-    // get data to be saved in an associative array using $request->only()
+    // get data to save in an associative array using $request->only()
     $data = $request->only(['title', 'description']);
 
     //  save post and assign return value of created post to $post array
     $post = Post::create($data);
+
+    // fire PostPublished event after post is successfully added to database
+    event(new PostPublished($post));
+    // or
+    // \Event::fire(new PostPublished($post))
 
     // return post as response, Laravel automatically serializes this to JSON
     return response($post, 201);
