10df2aca0bc0e967d2e2e062ad628241a6f1b6b2
manuelblanch <manuelblanch@iesebre.com>
2017-11-27 16:46:46 +0100

    testing

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,7 +4,26 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class PostController extends Controller
-{
-    //
+use App\Post;
+
+class PostController extends Controller {
+
+  /**
+   * Saves a new post to the database
+   */
+  public function store(Request $request) {
+    // ...
+    // validation can be done here before saving
+    // with $this->validate($request, $rules)
+    // ...
+
+    // get data to be saved in an associative array using $request->only()
+    $data = $request->only(['title', 'description']);
+
+    //  save post and assign return value of created post to $post array
+    $post = Post::create($data);
+
+    // return post as response, Laravel automatically serializes this to JSON
+    return response($post, 201);
+  }
 }
