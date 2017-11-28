bc534765e0770008195f8d4b168cd9f281351fee
manuelblanch <manuelblanch@iesebre.com>
2017-11-28 22:45:32 +0100

    notification

--- resources/assets/js/components/Home.vue
+++ resources/assets/js/components/Home.vue
@@ -17,3 +17,53 @@
     </div>
   </div>
 </template>
+<script>
+  export default {
+    data() {
+      return {
+        newPostTitle: "",
+        newPostDesc: ""
+      }
+    },
+    created() {
+      this.listenForChanges();
+    },
+    methods: {
+      addPost(postName, postDesc) {
+        // check if entries are not empty
+        if(!postName || !postDesc)
+          return;
+
+        // make API to save post
+        axios.post('/api/post', {
+          title: postName, description: postDesc
+        }).then( response => {
+          if(response.data) {
+            this.newPostTitle = this.newPostDesc = "";
+          }
+        })
+      },
+      listenForChanges() {
+        Echo.channel('posts')
+          .listen('PostPublished', post => {
+            if (! ('Notification' in window)) {
+              alert('Web Notification is not supported');
+              return;
+            }
+
+            Notification.requestPermission( permission => {
+              let notification = new Notification('New post alert!', {
+                body: post.title, // content for the alert
+                icon: "https://pusher.com/static_logos/320x320.png" // optional image url
+              });
+
+              // link to page on clicking the notification
+              notification.onclick = () => {
+                window.open(window.location.href);
+              };
+            });
+          })
+        }
+      }
+    }
+</script>
