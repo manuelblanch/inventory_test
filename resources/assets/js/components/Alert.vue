caa07da7e359a8b97f5fbf81823b01e84d8ee79f
manuelblanch <manuelblanch@iesebre.com>
2017-12-10 16:34:03 +0100

    notification

---
+++ resources/assets/js/components/Alert.vue
@@ -0,0 +1,64 @@
+<template>
+    <div class="alert" :class="[{'alert-dismissible' : isDismissable}, 'alert-' + color]" v-if="internalVisible">
+        <button v-if="isDismissable" type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+
+        <template v-if="!noTitle">
+            <slot name="title"><h4><i class="icon fa" :class="icon"></i> {{ title}}</h4></slot>
+        </template>
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'AdminLTEAlert',
+    data () {
+      return {
+        isDismissable: this.dismissable,
+        internalVisible: this.visible
+      }
+    },
+    computed: {
+      icon: function () {
+        switch (this.color) {
+          case 'info':
+            return 'fa-info'
+          case 'success':
+            return 'fa-check'
+          case 'danger':
+            return 'fa-ban'
+          case 'warning':
+            return 'fa-warning'
+          default :
+            return 'fa-info'
+        }
+      }
+    },
+    props: {
+      color: {
+        type: String,
+        default: 'info',
+        validator: value => {
+          let valid = ['', 'success', 'danger', 'warning', 'info']
+          return valid.includes(value)
+        }
+      },
+      visible: {
+        type: Boolean,
+        default: true
+      },
+      title: {
+        type: String,
+        default: 'Alert!'
+      },
+      dismissable: {
+        type: Boolean,
+        default: true
+      },
+      noTitle: {
+        type: Boolean,
+        default: false
+      }
+    }
+  }
+</script>
