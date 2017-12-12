6c23c5c01be275341011fe29634e4686abe5c169
manuelblanch <manuelblanch@iesebre.com>
2017-12-12 19:54:03 +0100

    test

---
+++ tests/Material_typeTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests;
+
+abstract class Material_TypeTest extends TestCase
+{
+    public function __construct()
+    {
+        // We have no interest in testing Eloquent
+      $this->mock = Mockery::mock('Eloquent', 'Material_Type_Model');
+    }
+
+    public function tearDown()
+    {
+        Mockery::close();
+    }
+
+    public function testIndex()
+    {
+        $this->mock
+           ->shouldReceive('all')
+           ->once()
+           ->andReturn('foo');
+
+        $this->app->instance('Material_Type_Model', $this->mock);
+
+        $this->call('GET', 'material_type_models');
+
+        $this->assertViewHas('material_type_models');
+    }
+}
