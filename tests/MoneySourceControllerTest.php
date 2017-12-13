62743026f3c38a98d0f64c00e9318ebac3ae70ec
manuelblanch <manuelblanch@iesebre.com>
2017-12-13 18:14:49 +0100

    test

---
+++ tests/MoneySourceControllerTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests;
+
+abstract class MoneySourceControllerTest extends TestCase
+{
+    public function __construct()
+    {
+        // We have no interest in testing Eloquent
+      $this->mock = Mockery::mock('Eloquent', 'MoneySourceController');
+    }
+
+    public function tearDown()
+    {
+        Mockery::close();
+    }
+
+    public function testIndex()
+    {
+        $this->mock
+           ->shouldReceive('all')
+           ->once()
+           ->andReturn('foo');
+        $this->app->instance('MoneySourceController', $this->mock);
+        $this->call('GET', 'moneysourcecontrollers');
+        $this->assertViewHas('moneysourcecontrollers');
+    }
+}
