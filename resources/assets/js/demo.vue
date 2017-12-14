d0eeecbb05b586d441586d69f4f8ee98586fa414
manuelblanch <manuelblanch@iesebre.com>
2017-12-14 19:05:03 +0100

    test

---
+++ resources/assets/js/demo.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+  <div class="content">
+    <section class="head">
+      <h1>Vue drag Verify</h1>
+      <h3>This is a vue component, which is sliding to unlock for login or sign up. This is used to protect your web app from bot attack.</h3>
+
+    </section>
+
+    <section class="demo-section">
+    <div class="column">
+      <drag-verify :width="width" :height="height" :text="text" :success-text="successText" :background="background" :progress-bar-bg="progressBarBg" :completed-bg="completedBg" :handler-bg="handlerBg" :handler-icon="handlerIcon" :text-size="textSize" :success-icon="successIcon" :circle="getShape"></drag-verify>
+    </div>
+
+      <div class="column">
+           <table>
+      <tr>
+          <td>width</td>
+          <td>
+            <input type="text" v-model="width">
+          </td>
+</tr>
+</template>
