7bf18b26e3f774a33745241c328ae2416abfe216
manuelblanch <manuelblanch@iesebre.com>
2017-12-22 16:59:39 +0100

    test

---
+++ resources/views/manteniments/vue/demo.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+  <div class="content">
+    <section class="head">
+      <h1>Vue drag Verify</h1>
+      <h3>This is a vue component, which is sliding to unlock for login or sign up. This is used to protect your web app from bot attack.</h3>
+
+    </section>
+
+    <section class="demo-section">
+    <div class="column">
+      <drag-verify :width="width" :height="height" :text="text" :success-text="successText" :background="background" :progress-bar-bg="progressBarBg" :completed-bg="completedBg" :handler-bg="handlerBg" :handler-icon="handlerIcon" :text-size="textSize" :success-icon="successIcon" :circle="getShape"></drag-verify>
+    </div>
+
+      <div class="column">
+           <table>
+      <tr>
+          <td>width</td>
+          <td>
+            <input type="text" v-model="width">
+          </td>
+        </tr>
+        <tr>
+          <td>height</td>
+          <td>
+            <input type="text" v-model="height">
+          </td>
+        </tr>
+        <tr>
+          <td>text</td>
+          <td>
+           <input type="text" v-model="text">
+          </td>
+        </tr>
+         <tr>
+          <td>successText</td>
+          <td>
+           <input type="text" v-model="successText">
+          </td>
+        </tr>
+        <tr>
+          <td>background</td>
+          <td>
+           <input type="color" v-model="background">
+          </td>
+        </tr>
+<tr>
