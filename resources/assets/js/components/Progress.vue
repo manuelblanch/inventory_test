a5853151cd23ea5c36b595022bd6eb3781e2c563
manuelblanch <manuelblanch@iesebre.com>
2018-02-02 16:32:34 +0100

    adding

--- resources/assets/js/components/Progress.vue
+++ resources/assets/js/components/Progress.vue
@@ -1,31 +1,28 @@
+
+
 <template>
 
-  <div class="progress" :class="[progressSize, isVertical?'vertical':'', isActive?'active':'']">
-    <div class="progress-bar"
-         :class="[`progress-bar-${type}`, (isStriped?'progress-bar-striped':'')]"
-         role="progressbar"
-         :aria-valuenow="value"
-         :aria-valuemin="min"
-         :aria-valuemax="max"
-         :style="`${isVertical?'height':'width'}: ${value}%`">
-      <span class="sr-only">{{text}}</span>
+<div class="progress" :class="[progressSize, isVertical?'vertical':'', isActive?'active':'']">
+    <div class="progress-bar" :class="[`progress-bar-${type}`, (isStriped?'progress-bar-striped':'')]" role="progressbar" :aria-valuenow="value" :aria-valuemin="min" :aria-valuemax="max" :style="`${isVertical?'height':'width'}: ${value}%`">
+        <span class="sr-only">{{text}}</span>
     </div>
-  </div>
+</div>
 
 </template>
 
 <script>
+
 export default {
-  name: 'ProgressBar',
-  props: {
-    type: {
-      type: String,
-      default: 'primary'
-    },
-    size: {
-      type: String,
-      default: ''
+    name: 'ProgressBar',
+    props: {
+        type: {
+            type: String,
+            default: 'primary'
+        },
+        size: {
+            type: String,
+            default: ''
+        }
     }
-}
 
 </script>
