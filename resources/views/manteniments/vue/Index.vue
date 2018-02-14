158c9c9194a28068e1b58bc8c4184361e76e7a18
manuelblanch <manuelblanch@iesebre.com>
2018-02-14 18:57:32 +0100

    testing

---
+++ resources/views/manteniments/vue/Index.vue
@@ -0,0 +1,43 @@
+
+<template>
+    <div>
+        <div class="form-group">
+            <router-link :to="{name: 'createBrand'}" class="btn btn-success">Crea una nova marca</router-link>
+        </div>
+
+        <div class="panel panel-default">
+            <div class="panel-heading">Marques</div>
+            <div class="panel-body">
+                <table class="table table-bordered table-striped">
+                    <thead>
+                    <tr>
+                        <th>Nom Curt</th>
+                        <th>Descripció</th>
+                        <th>Data Entrada</th>
+                        <th>Ultima Actualització</th>
+                        <th width="100">&nbsp;</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr v-for="brand, index in brands">
+                        <td>{{ brand.name }}</td>
+                        <td>{{ brand.description }}</td>
+                        <td>{{ brand.date }}</td>
+                        <td>{{ brand.update }}</td>
+                        <td>
+                            <router-link :to="{name: 'editBrand', params: {id: brand.id}}" class="btn btn-xs btn-default">
+                                Edit
+                            </router-link>
+                            <a href="#"
+                               class="btn btn-xs btn-danger"
+                               v-on:click="deleteEntry(brand.id, index)">
+                                Delete
+                            </a>
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</template>
--- src/Example.vue
+++ src/Example.vue
@@ -1,24 +1,25 @@
 <template>
-  <div class="container">
+
+<div class="container">
     <div class="box">
-      <div class="box-header">
-        <h2>Normal Alerts</h2>
-      </div>
-      <div class="box-body">
-        <va-alert>
-          <h4 slot="header">
+        <div class="box-header">
+            <h2>Normal Alerts</h2>
+        </div>
+        <div class="box-body">
+            <va-alert>
+                <h4 slot="header">
 <i class="icon fa fa-check"></i>
 
 <va-alert type="danger" :dismissible="true">
           <h4 slot="header">
             <i class="icon fa fa-check"></i>
           </h4>
-          <div slot="body">
-            Click on icon to check out it out on github.
-            <a href="" target="_blank">
-              <i class="fa fa-github fa-2x"></i>
-            </a>
-          </div>
-</va-alert>
+                <div slot="body">
+                    Click on icon to check out it out on github.
+                    <a href="" target="_blank">
+                        <i class="fa fa-github fa-2x"></i>
+                    </a>
+                </div>
+            </va-alert>
 
 </template>
