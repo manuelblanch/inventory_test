13621ed738131cd013e39d97f6b6a0c000d0ab31
manuelblanch <manuelblanch@iesebre.com>
2018-02-15 19:56:52 +0100

    testing

---
+++ resources/views/manteniments/vue/Create.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        <div class="form-group">
+            <router-link to="/" class="btn btn-default">Back</router-link>
+        </div>
+
+        <div class="panel panel-default">
+            <div class="panel-heading">Crea una nova marca</div>
+            <div class="panel-body">
+                <form v-on:submit="saveForm()">
+                    <div class="row">
+                        <div class="col-xs-12 form-group">
+                            <label class="control-label">Nom</label>
+                            <input type="text" v-model="brand.name" class="form-control">
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-xs-12 form-group">
+                            <label class="control-label">Descripció</label>
+                            <input type="text" v-model="brand.address" class="form-control">
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-xs-12 form-group">
+                            <label class="control-label">Data</label>
+                            <input type="text" v-model="brand.date" class="form-control">
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-xs-12 form-group">
+                            <label class="control-label">Ultima Actualització</label>
+                            <input type="text" v-model="brand.update" class="form-control">
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-xs-12 form-group">
+                            <button class="btn btn-success">Crear</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</template>
