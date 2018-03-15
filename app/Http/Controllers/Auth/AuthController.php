ede363e636fc24b33a9ce922719d8758f5cbde66
manuelblanch <manuelblanch@iesebre.com>
2018-03-15 20:03:15 +0100

    socialite

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -22,4 +22,20 @@ class AuthController extends Controller {
 
       }
 
+      protected function validator(array $data)
+
+    {
+
+        return Validator::make($data, [
+
+            'name' => 'required|max:255',
+
+            'email' => 'required|email|max:255|unique:users',
+
+            'password' => 'required|confirmed|min:6',
+
+        ]);
+
+    }
+
 }
