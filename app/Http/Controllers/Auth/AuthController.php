9737695600744102dd911a0bb98c7468878309ef
manuelblanch <manuelblanch@iesebre.com>
2018-03-15 19:56:46 +0100

    socialite

---
+++ app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+use App\User;
+use Validator;
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\ThrottlesLogins;
+use Illuminate\Foundation\Auth\AuthenticatesAndRegistersUsers;
+use Socialite;
+use Auth;
+use Exception;
+
+class AuthController extends Controller {
+
+  
+
+}
