598fadb58ca87197c7d100051c7d5537d6ae1b91
manuelblanch <manuelblanch@iesebre.com>
2018-03-15 19:48:56 +0100

    socialite

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -3,3 +3,13 @@
 Route::resource('itemCRUD', 'ItemCRUDController');
 Route::get('manage-vue', 'VueItemController@manageVue');
 Route::resource('vueitems', 'VueItemController');
+
+Route::get('linkedin', function () {
+
+    return view('linkedinAuth');
+
+});
+
+Route::get('auth/linkedin', 'Auth\AuthController@redirectToLinkedin');
+
+Route::get('auth/linkedin/callback', 'Auth\AuthController@handleLinkedinCallback');
