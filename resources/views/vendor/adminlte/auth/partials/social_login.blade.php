a124a08013c25a0ae72d5e0e558dd3752364b60f
manuelblanch <manuelblanch@iesebre.com>
2018-03-16 18:58:56 +0100

    socialite

--- resources/views/vendor/adminlte/auth/partials/social_login.blade.php
+++ resources/views/vendor/adminlte/auth/partials/social_login.blade.php
@@ -3,5 +3,5 @@
     <a href="{{ url('/auth/github') }}" class="btn btn-block btn-social btn-github btn-flat"><i class="fa fa-github"></i> {{ trans('adminlte_lang::message.signGithub') }}</a>
     <a href="{{ url('/auth/twitter') }}" class="btn btn-block btn-social btn-twitter btn-flat"><i class="fa fa-twitter"></i> {{ trans('adminlte_lang::message.signTwitter') }}</a>
     <a href="{{ url('/auth/facebook') }}" class="btn btn-block btn-social btn-facebook btn-flat"><i class="fa fa-twitter"></i> {{ trans('adminlte_lang::message.signFacebook') }}</a>
-
+    <a href="{{ url('/auth/limkedlin') }}" class="btn btn-block btn-social btn-linkedlin btn-flat"><i class="fa fa-twitter"></i> {{ trans('adminlte_lang::message.signFacebook') }}</a>
 </div><!-- /.social-auth-links -->
