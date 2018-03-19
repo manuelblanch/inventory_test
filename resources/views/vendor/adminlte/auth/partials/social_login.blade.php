619b355f9bf574300eca3bea6f9ef8aaa49fe07c
manuelblanch <manuelblanch@iesebre.com>
2018-03-19 20:06:05 +0100

    socialite

--- resources/views/vendor/adminlte/auth/partials/social_login.blade.php
+++ resources/views/vendor/adminlte/auth/partials/social_login.blade.php
@@ -2,6 +2,6 @@
     <p>- OR -</p>
     <a href="{{ url('/auth/github') }}" class="btn btn-block btn-social btn-github btn-flat"><i class="fa fa-github"></i> {{ trans('adminlte_lang::message.signGithub') }}</a>
     <a href="{{ url('/auth/twitter') }}" class="btn btn-block btn-social btn-twitter btn-flat"><i class="fa fa-twitter"></i> {{ trans('adminlte_lang::message.signTwitter') }}</a>
-    <a href="{{ url('/auth/facebook') }}" class="btn btn-block btn-social btn-facebook btn-flat"><i class="fa fa-twitter"></i> {{ trans('adminlte_lang::message.signFacebook') }}</a>
-    <a href="{{ url('/auth/limkedlin') }}" class="btn btn-block btn-social btn-linkedlin btn-flat"><i class="fa fa-twitter"></i> {{ trans('adminlte_lang::message.signFacebook') }}</a>
+    <a href="{{ url('/auth/facebook') }}" class="btn btn-block btn-social btn-facebook btn-flat"><i class="fa fa-facebook"></i> {{ trans('adminlte_lang::message.signFacebook') }}</a>
+    <a href="{{ url('/auth/linkedlin') }}" class="btn btn-block btn-social btn-linkedlin btn-flat"><i class="fa fa-linkedlin"></i> {{ trans('adminlte_lang::message.signFacebook') }}</a>
 </div><!-- /.social-auth-links -->
