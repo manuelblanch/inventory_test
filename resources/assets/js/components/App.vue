f401c17632ac6c1656b4ae76b6500a9c32d8fbcc
manuelblanch <manuelblanch@iesebre.com>
2018-04-11 19:29:56 +0200

    notification

---
+++ resources/assets/js/components/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <div class="heading">
+      <h1>Cruds</h1>
+    </div>
+    <crud-component
+      v-for="crud in cruds"
+      v-bind="crud"
+      :key="crud.id"
+      @update="update"
+      @delete="del"
+    ></crud-component>
+    <div>
+      <button @click="create()">Afegeix</button>
+    </div>
+  </div>
+</template>
+
+<script>
+  function Crud({ id, color, name}) {
+    this.id = id;
+    this.color = color;
+    this.name = name;
+  }
