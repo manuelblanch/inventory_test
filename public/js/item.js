768da82eb019826520e7ad5c49fa18db87120a1a
manuelblanch <manuelblanch@iesebre.com>
2018-04-16 18:31:50 +0200

    index

--- public/js/item.js
+++ public/js/item.js
@@ -19,3 +19,28 @@ new Vue({
     newItem : {'title':'','description':''},
     fillItem : {'title':'','description':'','id':''}
   },
+
+  computed: {
+         isActived: function () {
+             return this.pagination.current_page;
+         },
+         pagesNumber: function () {
+             if (!this.pagination.to) {
+                 return [];
+             }
+             var from = this.pagination.current_page - this.offset;
+             if (from < 1) {
+                 from = 1;
+             }
+             var to = from + (this.offset * 2);
+             if (to >= this.pagination.last_page) {
+                 to = this.pagination.last_page;
+             }
+             var pagesArray = [];
+             while (from <= to) {
+                 pagesArray.push(from);
+                 from++;
+             }
+             return pagesArray;
+         }
+     },
