2dcfa9783b5efbc0ceff600dae57e447956f1b38
manuelblanch <manuelblanch@iesebre.com>
2018-04-23 18:17:38 +0200

    composer

---
+++ tests/Unit/LoginTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class LoginTest extends TestCase {
+
+  public function add_user(){
+    
+  }
+
+}
+
+
+ ?>
