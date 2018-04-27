e3d2bfdb5949c3d61968c305daa3be066f14c67b
manuelblanch <manuelblanch@iesebre.com>
2018-04-27 20:34:13 +0200

    dashboard

--- resources/views/vendor/adminlte/home.blade.php
+++ resources/views/vendor/adminlte/home.blade.php
@@ -72,10 +72,10 @@
 	                            </li>
 
 															<a href="#">
-															                                    <div>
-															                                        <i class="fa fa-upload fa-fw"></i> Server Rebooted
-															                                        <span class="pull-right text-muted small">4 minutes ago</span>
-															                                    </div>
+															         <div>
+															         		<i class="fa fa-upload fa-fw"></i> Server Rebooted
+															               <span class="pull-right text-muted small">4 minutes ago</span>
+															         </div>
 															                                </a>
 															                            </li>
 															                            <li class="divider"></li>
@@ -117,6 +117,35 @@
                                         </button>
                                 </span>
 </div>
+
+</li>
+                            <li>
+                                <a href="index.html" class="active"><i class="fa fa-dashboard fa-fw"></i> Dashboard</a>
+                            </li>
+                            <li>
+                                <a href="#"><i class="fa fa-bar-chart-o fa-fw"></i> Charts<span class="fa arrow"></span></a>
+                                <ul class="nav nav-second-level">
+                                    <li>
+                                        <a href="flot.html">Flot Charts</a>
+                                    </li>
+                                    <li>
+                                        <a href="morris.html">Morris.js Charts</a>
+                                    </li>
+</ul>
+
+</li>
+													 <li>
+															 <a href="tables.html"><i class="fa fa-table fa-fw"></i> Tables</a>
+													 </li>
+													 <li>
+															 <a href="forms.html"><i class="fa fa-edit fa-fw"></i> Forms</a>
+													 </li>
+													 <li>
+															 <a href="#"><i class="fa fa-wrench fa-fw"></i> UI Elements<span class="fa arrow"></span></a>
+															 <ul class="nav nav-second-level">
+																	 <li>
+																			 <a href="panels-wells.html">Panels and Wells</a>
+</li>
 
 	<!-- ./col -->
 	<div class="col-lg-3 col-xs-6">
