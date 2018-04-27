f57488fe9ccc367c28eb719bf3dde8989fb2ef34
manuelblanch <manuelblanch@iesebre.com>
2018-04-27 19:40:57 +0200

    dashboard

--- resources/views/vendor/adminlte/home.blade.php
+++ resources/views/vendor/adminlte/home.blade.php
@@ -92,12 +92,12 @@
 															                            <i class="fa fa-user fa-fw"></i>Usuari<b class="caret"></b>
 															                        </a>
 															                        <ul class="dropdown-menu dropdown-user">
-															                            <li><a href="#"><i class="fa fa-user fa-fw"></i> User Profile</a>
+															                            <li><a href="#"><i class="fa fa-user fa-fw"></i>Perfil d'usuari</a>
 															                            </li>
-															                            <li><a href="#"><i class="fa fa-gear fa-fw"></i> Settings</a>
+															                            <li><a href="#"><i class="fa fa-gear fa-fw"></i>Opcions</a>
 															                            </li>
 															                            <li class="divider"></li>
-															                            <li><a href="login.html"><i class="fa fa-sign-out fa-fw"></i> Logout</a>
+															                            <li><a href="login.html"><i class="fa fa-sign-out fa-fw"></i>Logout</a>
 															                            </li>
 															                        </ul>
 															                    </li>
