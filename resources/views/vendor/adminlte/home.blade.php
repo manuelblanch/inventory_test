18af3c1350d49dbea7843eca80ee7cda3c3f29e4
manuelblanch <manuelblanch@iesebre.com>
2018-05-02 19:30:46 +0200

    dashboard

--- resources/views/vendor/adminlte/home.blade.php
+++ resources/views/vendor/adminlte/home.blade.php
@@ -515,6 +515,68 @@
                                         </div>
 </li>
 
+<li>
+                                        <div class="timeline-badge danger"><i class="fa fa-bomb"></i>
+                                        </div>
+                                        <div class="timeline-panel">
+                                            <div class="timeline-heading">
+                                                <h4 class="timeline-title">Lorem ipsum dolor</h4>
+                                            </div>
+                                            <div class="timeline-body">
+                                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus
+                                                    numquam
+                                                    facilis enim eaque, tenetur nam id qui vel velit similique nihil iure
+                                                    molestias aliquam, voluptatem totam quaerat, magni commodi quisquam.</p>
+                                            </div>
+                                        </div>
+                                    </li>
+                                    <li class="timeline-inverted">
+                                        <div class="timeline-panel">
+                                            <div class="timeline-heading">
+                                                <h4 class="timeline-title">Lorem ipsum dolor</h4>
+                                            </div>
+                                            <div class="timeline-body">
+                                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates est
+                                                    quaerat asperiores sapiente, eligendi, nihil. Itaque quos, alias
+                                                    sapiente
+                                                    rerum quas odit! Aperiam officiis quidem delectus libero, omnis ut
+                                                    debitis!</p>
+                                            </div>
+                                        </div>
+</li><li>
+                                        <div class="timeline-badge info"><i class="fa fa-save"></i>
+                                        </div>
+                                        <div class="timeline-panel">
+                                            <div class="timeline-heading">
+                                                <h4 class="timeline-title">Lorem ipsum dolor</h4>
+                                            </div>
+                                            <div class="timeline-body">
+                                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis minus
+                                                    modi
+                                                    quam ipsum alias at est molestiae excepturi delectus nesciunt, quibusdam
+                                                    debitis amet, beatae consequuntur impedit nulla qui! Laborum, atque.</p>
+                                                <hr>
+                                                <div class="btn-group">
+                                                    <button type="button" class="btn btn-primary btn-sm dropdown-toggle"
+                                                            data-toggle="dropdown">
+                                                        <i class="fa fa-gear"></i> <span class="caret"></span>
+                                                    </button>
+                                                    <ul class="dropdown-menu" role="menu">
+                                                        <li><a href="#">Action</a>
+                                                        </li>
+                                                        <li><a href="#">Another action</a>
+                                                        </li>
+                                                        <li><a href="#">Something else here</a>
+                                                        </li>
+                                                        <li class="divider"></li>
+                                                        <li><a href="#">Separated link</a>
+                                                        </li>
+                                                    </ul>
+                                                </div>
+                                            </div>
+                                        </div>
+</li>
+
 	<!-- ./col -->
 	<div class="col-lg-3 col-xs-6">
 		<!-- small box -->
