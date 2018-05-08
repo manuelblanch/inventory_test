0efa4722019f905734a5d9e661b54a97aa06ba64
manuelblanch <manuelblanch@iesebre.com>
2018-05-08 18:45:58 +0200

    calendar

---
+++ resources/views/manteniments/calendar/fullcalendar.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+
+@section('style')
+<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/2.2.7/fullcalendar.min.css"/>
+@endsection
