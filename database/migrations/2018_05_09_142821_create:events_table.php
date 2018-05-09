5816434e20ff2f82e7d01cd844549a0275ab3984
manuelblanch <manuelblanch@iesebre.com>
2018-05-09 16:30:51 +0200

    calendar

--- database/migrations/2018_05_09_142821_create:events_table.php
+++ database/migrations/2018_05_09_142821_create:events_table.php
@@ -3,8 +3,7 @@
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
-
-class Create:eventsTable extends Migration
+class CreateEventsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +12,14 @@ class Create:eventsTable extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('events', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->timestamps();
+        });
     }
-
     /**
      * Reverse the migrations.
      *
@@ -23,6 +27,6 @@ class Create:eventsTable extends Migration
      */
     public function down()
     {
-        //
+        Schema::drop("events");
     }
 }
