a2a367a6998d2e5d6283e6c7bc111480f66903c6
manuelblanch <manuelblanch@iesebre.com>
2018-05-13 16:59:53 +0200

    laravel social

---
+++ tests/Unit/BrandTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests;
+
+class BrandSpec extends TestCase {
+
+  function let(Repository $Repository)
+  {
+    $this->beConstructedWith($Repository);
+  }
+
+  function return_user_in_database(Repository $Repository)
+  {
+    $userRepository->get(1)->willReturn([
+        'id' => 1,
+        'firstname' => 'Marca',
+        'lastname' => 'Sound',
+      ])->shouldBeCalled();
+
+    $response = $this->get(1);
+
+    $response->shouldHaveType(User::class);
+    $response->id->shouldBe(1);
+    $response->firstname->shouldBe('Marca');
+    $response->lastname->shouldBe('Sound');
+  }
+
+  function throw_exception(Repository $Repository)
+  {
+    $Repository->get(1)->willReturn(null)->shouldBeCalled();
+
+    $this->shouldThrow(NoSuchUserException::class)->duringGet(1);
+  }
+}
