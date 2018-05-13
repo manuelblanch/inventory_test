2296ab0f558aec1d8b6d25918bd26248ae893c85
manuelblanch <manuelblanch@iesebre.com>
2018-05-13 17:19:40 +0200

    laravel social

--- resources/views/vendor/adminlte/auth/partials/social_login.blade.php
+++ resources/views/vendor/adminlte/auth/partials/social_login.blade.php
@@ -3,6 +3,7 @@
     <a href="{{ url('/auth/github') }}" class="btn btn-block btn-social btn-github btn-flat"><i class="fa fa-github"></i> {{ trans('adminlte_lang::message.signGithub') }}</a>
     <a href="{{ url('/auth/twitter') }}" class="btn btn-block btn-social btn-twitter btn-flat"><i class="fa fa-twitter"></i> {{ trans('adminlte_lang::message.signTwitter') }}</a>
     <a href="{{ url('/auth/facebook') }}" class="btn btn-block btn-social btn-facebook btn-flat"><i class="fa fa-facebook"></i> {{ trans('adminlte_lang::message.signFacebook') }}</a>
-    <a href="{{ url('/auth/linkedin') }}" class="btn btn-social-icon btn-linkedin"><i class="fa fa-linkedin"></i></a>
-    <a href="{{ url('/auth/linkedlin') }}" class="btn btn-block btn-social btn-linkedlin btn-flat"><i class="fa fa-linkedlin"></i> {{ trans('adminlte_lang::message.signFacebook') }}</a>
+    <a href="{{ url('/auth/linkedin') }}" class="btn btn-block btn-social btn-linkedin btn-flat"><i class="fa fa-linkedin"></i> {{ trans('adminlte_lang::message.signLinkedin') }}</a>
+    <a href="{{ url('auth/bitbucket') }}" class="btn btn-block btn-social btn-bitbucket btn-flat"><i class="fa fa-bitbucket"></i> {{ trans('adminlte_lang::message.signBitbucket') }}</a>
+    <a href="{{ url('auth/google') }}" class="btn btn-block btn-social-btn-google-plus btn-flat"><i class="fa fa-google-plus"></i> {{ trans('adminlte_lang::message.signGoogle') }}</a>
 </div><!-- /.social-auth-links -->
