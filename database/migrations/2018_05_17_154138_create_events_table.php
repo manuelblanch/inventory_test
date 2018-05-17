c5c25519afec1a309835a12820967ea81e9b2171
manuelblanch <manuelblanch@iesebre.com>
2018-05-17 17:45:19 +0200

    calendar

--- database/migrations/2018_05_17_154138_create_events_table.php
+++ database/migrations/2018_05_17_154138_create_events_table.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 class CreateEventsTable extends Migration
 {
@@ -29,6 +29,6 @@ class CreateEventsTable extends Migration
      */
     public function down()
     {
-        Schema::drop('events');
+        //
     }
 }
