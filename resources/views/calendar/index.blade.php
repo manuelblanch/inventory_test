7bcce4456bae3fa3f3eda42319171af38eccfe33
manuelblanch <manuelblanch@iesebre.com>
2018-05-18 18:12:23 +0200

    calendar

--- resources/views/calendar/index.blade.php
+++ resources/views/calendar/index.blade.php
@@ -1,16 +1,20 @@
 @extends('adminlte::layouts.app')
 
+@section('htmlheader_title')
+    inventary
+@endsection
+
 @section('style')
 <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/2.2.7/fullcalendar.min.css"/>
 @endsection
 
-@section('content')
+@section('main-content')
 <div class="container">
    <div class="row">
        <div class="col-md-8 col-md-offset-2">
            <div class="panel panel-default">
-               <div class="panel-heading">Full Calendar Example</div>
+               <div class="panel-heading">Calendari</div>
 
 <div class="panel-body">
                    {!! $calendar->calendar() !!}
--- resources/views/vendor/adminlte/layouts/app.blade.php
+++ resources/views/vendor/adminlte/layouts/app.blade.php
@@ -157,9 +157,6 @@ desired effect
       });
   </script>
 
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    
-
 @show
 
 </body>
