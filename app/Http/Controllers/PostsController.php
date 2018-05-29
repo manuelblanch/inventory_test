d3d47e49c9faca8ae0462d465caf56d0d49921c4
manuelblanch <manuelblanch@iesebre.com>
2018-05-29 21:12:21 +0200

    test

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -1,10 +1,22 @@
 <?php
 
-namespace App\Http\Controllers;
+# app/controllers/PostsController.php
 
-use Illuminate\Http\Request;
+class PostsController extends BaseController {
 
-class PostsController extends Controller
-{
-    //
+  protected $post;
+
+  public function __construct(Post $post)
+  {
+      $this->post = $post;
+  }
+
+  public function index()
+  {
+      $posts = $this->post->all();
+
+      return View::make('posts.index')
+          ->with('posts', $posts);
+  }
+
 }
---
+++ tests/Unit/PostControllerTest.php
@@ -0,0 +1,42 @@
+<?php
+
+# app/tests/controllers/PostsControllerTest.php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class PostControllerTest extends \PHPUnit_Framework_TestCase
+{
+    use WithoutMiddleware;
+
+    public function tearDown()
+    {
+        Mockery::close();
+    }
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->mock('');
+    }
+
+    public function mock($class)
+    {
+        $this->mock = Mockery::mock($class);
+
+        $this->app->instance($class, $this->mock);
+
+        return $this->mock;
+    }
+
+    public function testIndex()
+    {
+        $this->mock->shouldReceive('getPaginated')->once();
+
+        $response = $this->call('GET', 'people');
+
+        $this->assertResponseOk();
+    }
+}
