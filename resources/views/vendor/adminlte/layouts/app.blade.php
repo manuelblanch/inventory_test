56fa65c8ea925ae8f6b2c42e30a4f429cae52de2
manuelblanch <manuelblanch@iesebre.com>
2018-05-29 11:55:54 +0200

    notification

--- resources/views/vendor/adminlte/layouts/app.blade.php
+++ resources/views/vendor/adminlte/layouts/app.blade.php
@@ -224,6 +224,63 @@ desired effect
 	}
   </script>
 
+  <script>
+
+  $(document).ready(function(){
+
+    readProducts(); /* it will load products when document loads */
+
+    $(document).on('click', '#add_product', function(e){
+
+      var productId = $(this).data('id');
+      SwalAdd(productId);
+      e.preventDefault();
+    });
+
+  });
+
+  function SwalAdd(productId){
+
+    swal({
+      title: 'Estas?',
+      text: "El producte!",
+      type: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete it!',
+      showLoaderOnConfirm: true,
+
+      preConfirm: function() {
+        return new Promise(function(resolve) {
+
+           $.ajax({
+            url: 'resources/views/manteniments/location/delete.php',
+            type: 'POST',
+              data: 'delete='+productId,
+              dataType: 'json'
+           })
+           .done(function(response){
+            swal('Deleted!', response.message, response.status);
+          readProducts();
+           })
+           .fail(function(){
+            swal('Oops...', 'Something went wrong with ajax !', 'error');
+           });
+        });
+        },
+      allowOutsideClick: false
+    });
+
+  }
+
+  function readProducts(){
+    $('#load-products').load('resources/views/manteniments/location/index.blade.php');
+  }
+
+
+  </script>
+
 
 @show
 @isset($calendar_details)
