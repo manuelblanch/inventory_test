d9a15cdc00bb015ff87eac0478f34ec566c4a223
manuelblanch <manuelblanch@iesebre.com>
2018-06-03 21:24:28 +0200

    test

--- tests/Unit/ExportTest.php
+++ tests/Unit/ExportTest.php
@@ -8,25 +8,6 @@ use Mockery as m;
 
 class ExportTest extends \PHPUnit_Framework_TestCase
 {
-    public function setUp()
-    {
-        /*$this->afterApplicationCreated(function () {
-            $this->db = m::mock(
-                Connection::class.'[select,update,insert,delete]',
-                [m::mock(\PDO::class)]
-            );
-            $manager = $this->app['db'];
-            $manager->setDefaultConnection('mock');
-            $r = new \ReflectionClass($manager);
-            $p = $r->getProperty('connections');
-            $p->setAccessible(true);
-            $list = $p->getValue($manager);
-            $list['mock'] = $this->db;
-            $p->setValue($manager, $list);
-            $this->cityMock = m::mock(Export::class . '[update, delete]');
-        });
-        parent::setUp();*/
-    }
 
     public function setUp()
     {
