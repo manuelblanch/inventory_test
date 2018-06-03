6fcf49e9255f24a5903ed31fb8cb7a6a2a2a210c
manuelblanch <manuelblanch@iesebre.com>
2018-06-03 12:11:37 +0200

    test

--- tests/Unit/MoneySourceControllerTest.php
+++ tests/Unit/MoneySourceControllerTest.php
@@ -1,17 +1,33 @@
 <?php
 
 namespace Tests\Unit;
+use App\MoneySource;
+use App\User;
+use Config;
+use Illuminate\Contracts\Console\Kernel;
+use Tests\TestCase;
+
 
 class MoneySourceControllerTest extends \PHPUnit_Framework_TestCase
 {
-  public function testHome(){
-  $this->be(User::find(1));
-  $response = $this->call('GET', '/projects');
-  //CHECK IF AUTH USER
-  $this->assertTrue($response->isOk());
 
-  $this->assertViewHas('projects', 'expected value for projects');
-  $this->assertViewHas('users', 'expected value for users');
-  $this->assertViewHas('roles', 'expected value for roles');
+protected function setUp()
+{
+    parent::setUp();
+    
+}
+public function test1()
+{
+    $moneySource = MoneySource::create([
+        'name' => 'Elena',
+        'shortName' => 'El',
+        'description' => 'Noia'
+    ]);
+    $this->assertEquals($moneySource->fullname,'Elena, El, Noia');
+    $moneySource = MoneySource::create([
+        'shortName' => 'El',
+        'description' => 'Noia',
+    ]);
+    $this->assertEquals($moneySource->fullname,'El, Noia');
 }
 }
