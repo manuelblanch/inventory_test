e12eb81a4afbc64156894fc3df9c26690fdff32f
manuelblanch <manuelblanch@iesebre.com>
2018-06-04 21:02:00 +0200

    update

--- resources/views/manteniments/material_type/index.blade.php
+++ resources/views/manteniments/material_type/index.blade.php
@@ -88,42 +88,10 @@
     </div>
   </div>
 
-  <template>
-  <button v-on:click="alertDisplay">Click Me!</button>
-</template>
-
-
   <!-- /.box-body -->
 </div>
     </section>
     <!-- /.content -->
   </div>
 
-  <script>
-    export default {
-      data() {
-        return {}
-      },
-      methods: {
-        alertDisplay() {
-          this.$swal({
-            title: 'Are you sure?',
-            text: 'You can\'t revert your action',
-            type: 'warning',
-            showCancelButton: true,
-            confirmButtonText: 'Yes Delete it!',
-            cancelButtonText: 'No, Keep it!',
-            showCloseButton: true,
-            showLoaderOnConfirm: true
-          }).then((result) => {
-            if(result.value) {
-              this.$swal('Deleted', 'You successfully deleted this file', 'success')
-            } else {
-              this.$swal('Cancelled', 'Your file is still intact', 'info')
-            }
-          })
-        }
-      }
-    }
-  </script>
 @endsection
